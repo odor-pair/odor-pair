b98834983cb3f712756ce9af1f170bbeb6ef00d4
laurahsisson <laura.h.sisson@gmail.com>
2023-09-07 16:23:13 -0400

    showing mol

---
+++ main.py
@@ -0,0 +1,10 @@
+from rdkit import Chem
+from rdkit.Chem.Draw import MolToImage
+import pandas as pd
+
+# df = pd.read_csv("curated-solubility-dataset.csv")
+
+# print(df.columns)
+# molecule = Chem.MolFromSmiles(df["SMILES"][0])
+
+# MolToImage(molecule).show()
