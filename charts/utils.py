3665de533ac185092c9f67b3a0391b11187df3a0
Laura Sisson <laura.h.sisson@gmail.com>
2024-05-11 12:54:31 -0400

    Regressors and scoring charts improved

--- charts/utils.py
+++ charts/utils.py
@@ -6,11 +6,11 @@ def standard_fig_ax(x=1,y=1):
      return plt.subplots(x,y,figsize=(10,10))
     
 
-def save_figures(filename, fig=None, dpi_list=[300, 600], formats=['jpg', 'svg']):
+def save_figures(filename, fig, dpi_list=[300, 600], formats=['jpg', 'svg']):
     pathlib.Path(f'output/{filename}').mkdir(parents=True,exist_ok=True)
-    if fig is None:
-        fig = plt.gcf()
+    for text in fig.findobj(plt.Text):
+        text.set_fontweight('bold')
 
     for dpi in dpi_list:
         for format in formats:
-            fig.savefig(f'output/{filename}/{dpi}dpi.{format}', dpi=dpi, format=format)
+            fig.savefig(f'output/{filename}/{dpi}dpi.{format}', dpi=dpi, format=format, bbox_inches='tight')
